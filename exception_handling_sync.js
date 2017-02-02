aef5364dce9deba2289e92549b24073554e67e09
MRbcgd <quittypig@gmail.com>
2017-02-02 17:09:24 +0900

    비동기성 예외처리, try catch 문

---
+++ exception_handling_sync.js
@@ -0,0 +1,11 @@
+//비동기성 예외처리
+var fs=require('fs')
+
+console.log('--readFileSync--')//동기성 -> 순서대로
+
+try {
+  var text=fs.readFileSync('hellomynameisbakchulhyong.txt','utf8')
+  console.log(text)
+} catch (e) {
+  console.log(e)//파일이름 및 파일경로가 잘못되었다는 에러출력
+}
