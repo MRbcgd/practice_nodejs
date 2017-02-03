60022dce28b384f4f97a7c02fd9b94ec10a7da2c
MRbcgd <quittypig@gmail.com>
2017-02-03 14:16:33 +0900

    [볼필요없음]폴더정리

--- app.js
+++
@@ -1,5 +0,0 @@
-var rint=require('./rint.js')
-
-rint.timer.on('tick',function(){//이벤트리스너와 연결
-  console.log('tick..')
-})
--- util_module.js
+++
@@ -1,6 +0,0 @@
-//util 모듈의 format() 메소드
-//-> 출력하지 않고 문자열로 반환
-var util=require('util')
-
-var data=util.format('%d + %d + %d',10,20,10+20)
-console.log(data)
