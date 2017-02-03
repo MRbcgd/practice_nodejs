6911ed05c8daa913e3a0f08d6f3f2b3f55f4e068
MRbcgd <quittypig@gmail.com>
2017-02-03 15:41:53 +0900

    Content-Type -> MIME형식(MP3,JPG)

--- fileSystem_http.js
+++ fileSystem_http.js
@@ -11,3 +11,27 @@ require('http').createServer(function(request,response){
 }).listen(52273,function(){
     console.log('running..http://127.0.0.1:52273/')
 })
+
+require('http').createServer(function(request,response){
+  fs.readFile('picture.jpg',function(error,data){
+    if(error){
+      return console.log(error)
+    }
+    response.writeHead(200,{'Content-Type':'image/jpg'})//jpg파일
+    response.end(data)
+  })
+}).listen(52274,function(){
+    console.log('running..http://127.0.0.1:52274/')
+})
+
+require('http').createServer(function(request,response){
+  fs.readFile('music.mp3',function(error,data){
+    if(error){
+      return console.log(error)
+    }
+    response.writeHead(200,{'Content-Type':'audio/mp3'})//mp3파일
+    response.end(data)
+  })
+}).listen(52275,function(){
+    console.log('running..http://127.0.0.1:52275/')
+})
